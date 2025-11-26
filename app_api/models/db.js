5b0d5884a5fb946d268877a33f18b8f720c8372e
receparslan42 <receparslan965@gmail.com>
2025-11-26 21:59:38 +0300

    Backend

---
+++ app_api/models/db.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+// Database URI
+//var dbURI = "mongodb://localhost/add_venue";
+var dbURI = "mongodb+srv://test:test@addvenue.bh1dnqj.mongodb.net/"
+mongoose.connect(dbURI);
+
+// When successfully connected
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose connected to " + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose connection error: " + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose disconnected");
+});
+
+// Capture app termination / restart events
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
+    console.log("Mongoose disconnected through app termination");
+    process.exit(0);
+  });
+});
+
+require("./venue");
